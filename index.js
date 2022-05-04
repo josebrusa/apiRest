1184c788776dc28f6207ad52c302c4e9feae884f
jose brusa <josebrusa@outlook.com>
2022-05-04 12:16:21 +0200

    add style card and view 3 img

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 console.log('Happy Coding!!! 👨‍💻👨‍💻👨‍💻');
 
-const API_URL = 'https://api.thecatapi.com/v1/images/search';
+
+const limit = 'limit=3';
+const pages = 'page=2';
+const API_URL = `https://api.thecatapi.com/v1/images/search?${limit}&${pages}`;
 
 // fetch(URL)
 //     .then(res => res.json())
@@ -16,8 +19,13 @@ const reload = async () => {
     const res = await fetch(API_URL);
     const data = await res.json();
     console.log(data)
-    const img = document.querySelector('img');
-    img.src = data[0].url;
+    const img1 = document.getElementById('img1');
+    const img2 = document.getElementById('img2');
+    const img3 = document.getElementById('img3');
+    
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+    img3.src = data[2].url;
 
 }
 
